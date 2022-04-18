26009696c0f40887f04dce70756f589421dca322
Christos <987149+christosle@users.noreply.github.com>
2022-04-18 22:27:56 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,152 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'MemAssist',
+  tagline: 'Cache Memory Optimization Tool',
+  url: 'https://memassist.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'memassist', // Usually your GitHub org/user name.
+  projectName: 'memassist.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/memassist/memassist.github.io/edit/main',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '1.x.x',
+            },
+          },
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/memassist/memassist.github.io/edit/main/...',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        googleAnalytics: {
+          trackingID: 'UA-186931777-2',
+          anonymizeIP: true,
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      metadata: [{name: 'keywords', content: 'memassist, reuse distance analysis, locality optimization, source code analysis'}],
+      hideableSidebar: true,
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      navbar: {
+        title: 'MemAssist',
+        logo: {
+          alt: 'MemAssist Logo',
+          src: 'img/logo.png',
+        },
+        hideOnScroll: true,
+        items: [
+          {
+            type: 'doc',
+            docId: 'tutorial/intro',
+            position: 'right',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://memassist.lezos.gr/',
+            label: 'Demo',
+            position: 'right',
+          },
+          // {
+          //   href: 'https://github.com/memassist',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+          {
+            href: 'https://github.com/memassist',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository'
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'Community',
+          //   items: [
+          //     {
+          //       label: 'Stack Overflow',
+          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //     },
+          //     {
+          //       label: 'Discord',
+          //       href: 'https://discordapp.com/invite/docusaurus',
+          //     },
+          //     {
+          //       label: 'Twitter',
+          //       href: 'https://twitter.com/docusaurus',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/memassist',
+          //     },
+          //   ],
+          // },
+        ],
+        logo: {
+          alt: 'MemAssist Logo',
+          src: '/img/logo.png',
+          href: 'https://memassist.github.io',
+        },
+        copyright: `Copyright © ${new Date().getFullYear()} MemAssist.`,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+//import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useBaseUrl from '@docusaurus/useBaseUrl'
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <div>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+
+        <div className={clsx('margin-top--md', styles.buttons)}>
+          <a
+            className={clsx('button button--secondary button--lg margin--sm', styles.githubButton)}
+            href="https://github.com/memassist"
+            target="_blank">
+            <span className={styles.githubIcon}></span> <span>GitHub</span>
+          </a>
+          <a
+            className={clsx('button button--secondary button--lg margin--sm')}
+            href="https://memassist.lezos.gr/"
+            target="_blank"
+            title="MemAssist Demo">
+            Demo of the Tool
+          </a>
+          <a
+            className="button button--secondary button--lg  margin--sm"
+            href="https://dl.acm.org/doi/10.1145/3398189?cid=99658696775"
+            target="_blank"
+            title="A Locality Optimizer for Loop-Dominated Applications Based on Reuse Distance Analysis">
+            Read the Publication
+          </a>
+        </div>
+
+        {/*
+        <div className={clsx('margin-top--md', styles.buttons)}>
+          <Link
+            className="button button--secondary button--lg  margin--sm"
+            to={useBaseUrl('docs/tutorial/intro')}>
+            ⏱ Tutorial - 5min
+          </Link>
+          <a
+            className="button button--secondary button--lg  margin--sm"
+            href="https://dl.acm.org/doi/10.1145/3398189?cid=99658696775"
+            target="_blank"
+            title="A Locality Optimizer for Loop-Dominated Applications Based on Reuse Distance Analysis">
+            Read the Publication
+          </a>
+        </div>
+        */}
+      </div>
+    </header>
+  );
+}
+
+function HomepageMain() {
+  return (
+    <section className={clsx('margin-top--md', styles.main)}>
+      <div className="container">
+        <h2>Optimize your algorithms in terms of data locality</h2>
+        <p className={styles.descriptionText}>
+          <strong>MemAssist</strong> is a compiler assisted dynamic code analysis tool that supports the
+          optimization of application code in C or MATLAB. It is used to indicate source code refactorings/optimizations that may improve
+          temporal locality and reduce cache misses on the targeted architecture cache(s).
+          The tool has been primarily deployed as an extension for the Visual Studio IDE
+          but is now offered as a web application for hands on evaluation.
+        </p>
+        <p>
+          For more information on MemAssist and how it works refer to:
+        </p>
+        <figure>
+          <blockquote className={styles.blockquote}>
+            <p>
+              <a href="https://dl.acm.org/doi/10.1145/3398189?cid=99658696775"
+                 target="_blank"
+                 title="A Locality Optimizer for Loop-Dominated Applications Based on Reuse Distance Analysis">
+                A Locality Optimizer for Loop-Dominated Applications Based on
+                Reuse Distance Analysis
+              </a>
+            </p>
+          </blockquote>
+          <figcaption className={styles.blockquoteFooter}>
+            Christakis Lezos, Grigoris Dimitroulakos, Ioannis Latifis, Konstantinos Masselos
+            <br/>
+            <cite title="ACM TODAES">ACM Transactions on Design Automation of Electronic Systems (TODAES)</cite>,
+            Volume 25 Issue 6, September 2020
+          </figcaption>
+        </figure>
+        <p className={clsx('margin-top--lg')}>
+          <em>
+            Free access to the PDF is provided through the ACM Author-izer service.
+            <br/>
+            By clicking the link above, you will be transferred to the ACM Digital
+            Library page of the publication where the PDF download link will be automatically unlocked.
+          </em>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description={`${siteConfig.tagline}`}>
+      <HomepageHeader />
+      <main>
+        {/*<HomepageFeatures />*/}
+        <HomepageMain />
+      </main>
+    </Layout>
+  );
+}
